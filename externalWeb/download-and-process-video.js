526d9a300ab51dd7c42a57521439dc62db0e8bb5
yamodah <yassinedahlek@gmail.com>
2021-09-23 16:19:10 -0400

    fixed order of console.log and removed set interval was going to never stop

--- externalWeb/download-and-process-video.js
+++ externalWeb/download-and-process-video.js
@@ -1,10 +1,12 @@
-function download(url) {
+function download(url, callback) {
     console.log(`Start downloading video from ${url} ...`);
   
     setTimeout(() => {
       const fileName = url.split("/").pop();
       console.log(`Video downloaded from ${url} to ${fileName}.`);
-    }, 2000);
+      callback(fileName);
+    }, 3000);
+    
   }
   
   function process(videoFile) {
@@ -17,6 +19,6 @@ function download(url) {
   
   const url = "https://www.thinkful.com/sync-and-async.mov";
   
-  const fileName = download(url);
+  download(url, process);
+
   
-  process(fileName);
